bf760b2752774e18eb68d4d4b072be8175046add
Wonsik Shin <mmaring0120@gmail.com>
2024-08-26 06:18:01 +0000

    feat: allow rolling multiple dice

--- src/commands/dice.ts
+++ src/commands/dice.ts
@@ -4,12 +4,35 @@ import { SlashCommand } from '@/types';
 const command: SlashCommand = {
   data: new SlashCommandBuilder()
     .setName('dice')
-    .setDescription('Rolls a six-sided die.'),
+    .setDescription('Rolls one or more six-sided dice.')
+    .addIntegerOption((option) =>
+      option
+        .setName('n')
+        .setDescription('How many six-sided dice to roll. (1-6)')
+        .setMinValue(1)
+        .setMaxValue(6),
+    ) as SlashCommandBuilder,
   global: true,
   execute: async (interaction: ChatInputCommandInteraction) => {
     const user = interaction.user;
-    const roll = Math.floor(Math.random() * 6) + 1;
-    await interaction.reply(`${user} rolled **${roll}**.`);
+    const numberOfDice = interaction.options.getInteger('n') || 1;
+
+    const rolls = Array.from(
+      { length: numberOfDice },
+      () => Math.floor(Math.random() * 6) + 1,
+    );
+    const totalRoll = rolls.reduce((sum, roll) => sum + roll, 0);
+
+    let replyMessage = `${user} rolled `;
+
+    if (numberOfDice === 1) {
+      replyMessage += `**${rolls[0]}**.`;
+    } else {
+      const rollsString = rolls.map((roll) => `**${roll}**`).join(', ');
+      replyMessage += `${rollsString} for a total of **${totalRoll}**.`;
+    }
+
+    await interaction.reply(replyMessage);
   },
 };
 
